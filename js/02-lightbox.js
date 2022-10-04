d3ba7614726c812d963fd5d9faae06c3ceecc90f
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-10-04 20:43:14 +0200

    finish task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+for (let item of galleryItems) {
+    
+    let newLink = document.createElement("a");
+    newLink.href = item.original;
+    newLink.classList.add("gallery__link");
+
+    let newImage = document.createElement("img");
+    newImage.classList.add("gallery__image");
+    newImage.src = item.preview;
+    newImage.alt = item.description;
+
+    newLink.append(newImage); 
+    gallery.append(newLink); 
+   
+}
+
+let lightboxElement = new SimpleLightbox('.gallery__link', {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
+lightboxElement.on('show.simplelightbox', function () {
+    
+});
+
+
+gallery.addEventListener("click", (e) => {
+    e.preventDefault();  
+})
+
+
+
+		
+
+	
+
